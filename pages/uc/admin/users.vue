be6c192cda4c1b491642ac4832a7cd8364a869b0
walkinglf <793061804@qq.com>
2025-04-03 21:58:05 +0800

    支持记录活跃信息

--- pages/uc/admin/users.vue
+++ pages/uc/admin/users.vue
@@ -56,12 +56,12 @@
           </el-table-column>
           <el-table-column prop="email" label="邮箱地址" min-width="200" align="center" />
           <el-table-column prop="intro" label="个人介绍" show-overflow-tooltip min-width="200" />
-          <el-table-column prop="loginIp" label="登录地址" align="center" width="120">
+          <el-table-column prop="lastActiveIp" label="最后活跃地址" align="center" width="120">
             <template #default="scope">
               <el-tooltip
                 class="box-item"
                 effect="dark"
-                :content="scope.row.loginIp"
+                :content="scope.row.lastActiveIp"
                 placement="top-start"
               >
                 <el-text>{{ scope.row.location }}</el-text>
@@ -89,7 +89,7 @@
             align="center"
             width="150"
           />
-          <el-table-column prop="loginDate" label="登录时间" align="center" width="150" />
+          <el-table-column prop="lastActiveDate" label="最后活跃时间" align="center" width="150" />
           <el-table-column fixed="right" label="操作" width="150" align="center">
             <template #default="scope">
               <el-button type="primary" text bg size="small" @click="updateUser(scope.row)">
